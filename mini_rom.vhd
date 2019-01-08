736b6f287e143371efd3cdf3ee1d46c6bef15f48
RenanPicoli <renanps123@gmail.com>
2019-01-07 22:25:12 -0200

    changed memory location where values are loaded from

--- mini_rom.vhd
+++ mini_rom.vhd
@@ -22,12 +22,12 @@ architecture memArch of mini_rom is
 
 	type memory is array (0 to 15) of std_logic_vector(31 downto 0);
 	constant rom: memory := (--asm approx. follows Intel syntax: destination before source
-	x"8C01_0004",--lw [r0+4] r1
+	x"8C01_0000",--lw [r0+0] r1
 	x"4000_0000",--xori r0 r0 0x0000 (nop)
-	x"8C02_0008",--lw [r0+8] r2
+	x"8C02_0004",--lw [r0+4] r2
 	x"0041_1820",--add r3 r2 r1
-	x"AC03_000C",--sw [r0+12] r3 armazena soma na memória
-	x"8C03_000C",--lw [r0+12] r3 confere a soma
+	x"AC03_0008",--sw [r0+8] r3 armazena soma na memória
+	x"8C03_0008",--lw [r0+8] r3 confere a soma
 	x"0800_0000",--jmp 0
 	others => x"0800_0007" --jmp 7
 	);
