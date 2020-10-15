1584ecf0f995317f590df26eb5cc282dde79de14
RenanPicoli <renanps123@gmail.com>
2020-10-14 21:39:22 -0300

    added ENA (input that enables writes) to D flip flop; simplified logic of register file: demux uses ENA to select which register to write to.

--- d_flip_flop.vhd
+++ d_flip_flop.vhd
@@ -13,6 +13,7 @@ entity d_flip_flop is
 
 port(	D:	in std_logic_vector(31 downto 0);
 		RST:	in std_logic;--synchronous reset
+		ENA:	in std_logic:='1';--enables writes
 		CLK:	in std_logic;
 		Q:	out std_logic_vector(31 downto 0)  
 );
@@ -30,7 +31,7 @@ begin
 	if (CLK'event and CLK = '1') then
 		if (RST='1') then
 			Q <= (others=>'0');
-		else
+		elsif (ENA = '1') then
 			Q <= D;
 		end if;
 	end if;
