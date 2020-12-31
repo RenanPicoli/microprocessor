8e8e67726e56216eb90a34cce3b86e84427f542f
RenanPicoli <renanps123@gmail.com>
2020-12-31 12:19:43 -0300

    made mini_ram a generic component (addresses 2^N words); stores constants for adaptive filter program

--- mini_ram.vhd
+++ mini_ram.vhd
@@ -10,9 +10,12 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;--to_integer
 
+use work.my_types.all;
+
 entity mini_ram is
+	generic (N: integer);--size in bits of address 
 	port (CLK: in std_logic;--borda de subida para escrita, memória pode ser lida a qq momento desde que rden=1
-			ADDR: in std_logic_vector(4 downto 0);--addr é endereço de byte, mas os Lsb são 00
+			ADDR: in std_logic_vector(N-1 downto 0);--addr é endereço de byte, mas os Lsb são 00
 			write_data: in std_logic_vector(31 downto 0);
 			rden: in std_logic;--habilita leitura
 			wren: in std_logic;--habilita escrita
@@ -22,40 +25,24 @@ end mini_ram;
 
 architecture memArch of mini_ram is
 
-	type memory is array (0 to 31) of std_logic_vector(31 downto 0);
-	constant initial_values: memory := (
---	0 => x"FFFF_FFFE",-- (-2)
---	0 => x"0000_0002",-- 2
---	1 => x"FFFF_FFF6",-- (-10)
---	1 => x"0000_0003",-- +3
---	0 => x"3FC00000",-- 1.5
---	1 => x"3B03126F",-- 0.002
---	0=> x"435C0000",--220
---	1=> x"435D0000",--221
-	0=> x"40000000",--2.0 a filter constant
-	1=> x"3F800000",--1.0
-	2=> x"3F800000",--1.0
-	3=> x"3F800000",--1.0
-	4=> x"3F800000",--1.0
-	5=> x"3F800000",--1.0
-	6=> x"3F800000",--1.0
-	7=> x"3F800000",--1.0
-	8=> x"3F800000",--1.0
---	2=> x"40000000",--2.0
---	3=> x"40400000",--3.0
---	4=> x"40800000",--4.0
+--	type memory is array (0 to 2**N-1) of std_logic_vector(31 downto 0);
+	constant initial_values: array32 (0 to 2**N-1) := (
+	0=> x"3851B717",--5E-5: a filter constant
+	1=> x"7FFFFFFF",-- constant for comparison
+	2=> x"3F000000",-- 0.5
+	3=> x"3F800000",-- 1.0
 	others 	=> x"0000_0000"
 	);
 	
 	--lembrar de desabilitar auto RAM replacement em compiler settings>advanced settings (synthesis)
-	signal possible_outputs: memory := initial_values;
+	signal possible_outputs: array32 (0 to 2**N-1) := initial_values;
 	
 	begin
 		--write behaviour:
 		write_proc: process(CLK,wren)
 		begin
 		if (CLK'event and CLK='1') then
-			if (wren='1') then
+			if (wren='1') then --normal write operation
 				possible_outputs(to_integer(unsigned(ADDR))) <= write_data;
 			end if;
 		end if;
