8eda1ad8baae4ab5c9a64ab899ce8c2060b3e157
RenanPicoli <renanps123@gmail.com>
2021-08-09 17:36:10 -0300

    my_types.vhd: created a type for arrays of 32bit words

--- my_types.vhd
+++ my_types.vhd
@@ -3,6 +3,7 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all; 
 
 package my_types is
+	type array_of_std_logic_vector is array (natural range <>) of std_logic_vector(31 downto 0);
 	type array32 is array (natural range <>) of std_logic_vector (31 downto 0);
 	type array4 is array (natural range <>) of std_logic_vector (3 downto 0);
 	type array7 is array (natural range <>) of std_logic_vector (6 downto 0);
