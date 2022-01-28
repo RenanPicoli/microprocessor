3d1d46e64050c60f43f40f76963fd20fc4933e53
RenanPicoli <renanps123@gmail.com>
2022-01-28 08:25:12 -0300

    parallel_load_cache: removed ram behavior to decrease logic consumption (now only parallel transfers are allowed - flip flops are inferred)

--- parallel_load_cache.vhd
+++ parallel_load_cache.vhd
@@ -38,20 +38,25 @@ architecture memArch of parallel_load_cache is
 		write_proc: process(CLK,RST,wren,parallel_wren)
 		begin
 		if (rising_edge(CLK)) then
-			if (RST='1') then
-				possible_outputs <= (others=>(others=>'0'));
-			elsif (wren='1' and parallel_wren='0') then --normal write operation (single word)
-				possible_outputs(to_integer(unsigned(ADDR))) <= write_data;
-			elsif (parallel_wren='1') then--processor doesn't know exactly when it is going to happen a parallel write
+--			if (RST='1') then
+--				possible_outputs <= (others=>(others=>'0'));
+--			elsif (wren='1' and parallel_wren='0') then --normal write operation (single word)
+--				possible_outputs(to_integer(unsigned(ADDR))) <= write_data;
+--			elsif (parallel_wren='1') then--processor doesn't know exactly when it is going to happen a parallel write
+			if (parallel_wren='1') then--processor doesn't know exactly when it is going to happen a parallel write
 				possible_outputs <= parallel_write_data;
 			end if;
+			--Q connects to a top level mux, no need for output enable
+--			Q <= possible_outputs(to_integer(unsigned(ADDR)));--old data read-during-write
+																		
+			--output behaviour:
+			--parallel_read_data connects to a shared data bus
+--			if(parallel_rden='1')then
+--				parallel_read_data <= possible_outputs;--old data read-during-write
+--			else
+--				parallel_read_data <= (others=>(others=>'Z'));
+--			end if;
 		end if;
 		end process;
-																		
-		--output behaviour:
-		--parallel_read_data connects to a shared data bus
-		parallel_read_data <= 	possible_outputs when parallel_rden='1' else
-										(others=>(others=>'Z'));
-		--Q connects to a top level mux, no need for output enable
-		Q <= possible_outputs(to_integer(unsigned(ADDR)));
+		parallel_read_data <= possible_outputs;--asynchronous read logic
 end memArch;
