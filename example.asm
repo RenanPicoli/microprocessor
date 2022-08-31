e248fa60326a17a30a6117f464768c959768b45a
RenanPicoli <renanps123@gmail.com>
2022-08-30 23:25:30 -0300

    example.asm: changed identation

--- example.asm
+++ example.asm
@@ -1,21 +1,21 @@
 .section data
-    BASE 20;
-    MAX_HEIGHT 30;
+  BASE 20;
+  MAX_HEIGHT 30;
 	THICKNESS xA4;
 .section code
-    xor r0 r0 r0;
-	halt;
-    addi r0 r0 BASE;
-    xor r1 r1 r1;
-    addi r1 r1 MAX_HEIGHT;
-    push r0; passes 1st argument
-    push r1; passes 2nd argument
-    call FOO;
-    ldrv r2;
-    ret;
+  xor r0 r0 r0;
+	nop;
+  addi r0 r0 BASE;
+  xor r1 r1 r1;
+  addi r1 r1 MAX_HEIGHT;
+  push r0; passes 1st argument
+  push r1; passes 2nd argument
+  call FOO;
+  ldrv r2;
+  ret;
 FOO:
-    pop r1;
-    pop r0;
-    mult r0 r1; [hi lo] <= r0*r1
-    mflo r2; r2 <= lo (discards MSB)
-    ret;
+  pop r1;
+  pop r0;
+  mult r0 r1; [hi lo] <= r0*r1
+  mflo r2; r2 <= lo (discards MSB)
+  ret;
