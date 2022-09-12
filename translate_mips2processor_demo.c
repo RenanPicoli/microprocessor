a2af341071328c2a644ec36b06665a009c80deaa
RenanPicoli <renanps123@gmail.com>
2022-09-12 17:16:05 -0300

    translate_mips2processor_demo.c: now it iterates over a file to process its instructions

--- translate_mips2processor_demo.c
+++ translate_mips2processor_demo.c
@@ -2,36 +2,82 @@
 #include <stdlib.h>
 #include <string.h>
 
+//maximum length of line in text files
+#define MAX_STR_LENGTH 200
+
 char *replace_char(char *str, char find, char replace);
 
-int main()
+int main(int argc,char* argv[])
 {
+	FILE *fp;//file pointer for read
+	FILE *of;//file pointer for outputfile
+
+	if(argc != 2){
+		printf("Uso: assembler FILE\n");
+		return 1;
+	}
+
+	of=fopen("./main.s","w");// opens a text file for write
+	if(of==NULL){
+		printf("Erro ao criar o arquivo de saída!\n");
+		return 1;
+	}
+	
 	//char instr[] = "lw $4,8($5)";
-	char instr[] = "fadd $1,$2,$3";
+	//char instr[] = "fadd $1,$2,$3";
 	//char instr[] = "mult $2,$2";
+	char* instr=calloc(MAX_STR_LENGTH,sizeof(char));//temporary string to store instructions read from files
+	const char* beginning_of_allocated_str = instr;
+
+	fp=fopen(argv[1],"r");//instructions will only be read
+	if(fp==NULL){
+		printf("Erro ao ler o arquivo %s\n",argv[1]);
+		return 2;
+	}
+
+
+	printf("Parsing %s\n",argv[1]);
+
 	char *new_instr = (char *)calloc(20, sizeof(char));
 	char *opcode = (char *)calloc(6, sizeof(char));
-	sscanf(instr, "%[a-zA-Z] %*s", opcode);
 
-	printf("%s -> ", instr);
-	int offset = 0;
-	char arg1[4];
-	char arg2[4];
-	char arg3[4];
-	if (strcmp(opcode, "sw") == 0 || strcmp(opcode, "lw") == 0)
-	{
-		//printf("Found load-store instruction");
-		sscanf(instr, "%[a-zA-Z] %[$a-zA-Z0-9] , %d ( %[$a-zA-Z0-9] ) ", opcode, arg1, &offset, arg2);
-		sprintf(new_instr, "%s \[%s+%d\] %s", opcode, arg2, offset, arg1);
+	while (!feof(fp)){
+		char* fgets_retval=fgets((char*)instr,MAX_STR_LENGTH,fp);//reads a single line of fp, terminated with '\n', expects at most 199 chars
+		if(fgets_retval!=NULL){
+			//removes initial blank spaces
+			while(1){
+				if(instr[0]=='\t'||instr[0]==' '){
+					instr++;
+				}else{
+					break;
+				}
+			}
+
+			sscanf(instr, "%[a-zA-Z] %*s", opcode);
+
+			printf("%s -> ", instr);
+			int offset = 0;
+			char arg1[4];
+			char arg2[4];
+			char arg3[4];
+			if (strcmp(opcode, "sw") == 0 || strcmp(opcode, "lw") == 0)
+			{
+				//printf("Found load-store instruction");
+				sscanf(instr, "%[a-zA-Z] %[$a-zA-Z0-9] , %d ( %[$a-zA-Z0-9] ) ", opcode, arg1, &offset, arg2);
+				sprintf(new_instr, "%s \[%s+%d\] %s", opcode, arg2, offset, arg1);
+			}
+			else
+			{ // R-type: add,sub,and,or,xor,nor,fadd,fmul,fdiv,fsub
+				sscanf(instr, "%[a-zA-Z] %[$a-zA-Z0-9] , %[$a-zA-Z0-9] , %[$a-zA-Z0-9]", opcode, arg1, arg2, arg3);
+				sprintf(new_instr, "%s %s %s %s", opcode, arg2, arg3, arg1);
+			}
+			replace_char(new_instr, '$', 'r');
+			strcat(new_instr, ";");
+			printf("%s\n", new_instr);
+		}
 	}
-	else
-	{ // R-type: add,sub,and,or,xor,nor,fadd,fmul,fdiv,fsub
-		sscanf(instr, "%[a-zA-Z] %[$a-zA-Z0-9] , %[$a-zA-Z0-9] , %[$a-zA-Z0-9]", opcode, arg1, arg2, arg3);
-		sprintf(new_instr, "%s %s, %s, %s", opcode, arg2, arg3, arg1);		
-	}
-	replace_char(new_instr, '$', 'r');
-	strcat(new_instr, ";");
-	printf("%s\n", new_instr);
+
+	free(beginning_of_allocated_str);
 	return 0;
 }
 
