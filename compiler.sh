f109b5996bacd83e1f8c9a8568d75d35905be8e4
RenanPicoli <renanps123@gmail.com>
2022-09-29 17:36:32 -0300

    Added compiler.sh: a script to automate the whole process of compilation of C code to MIPS, translation to my assembly and generation of binary executable

---
+++ compiler.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+echo "number of arguments: $#"
+
+# checks if arguments were provided
+if [ $# -ne 1 ];
+then
+	# $1 source file to be compiled
+	echo "Usage $0 SOURCE_FILE"
+	exit 1
+fi
+
+# mips-linux-gnu-gcc output file repeats source file name but changes extension to ".s"
+gcc_out="${1%.*}.s"
+
+echo "gcc for mips will be at file $gcc_out"
+
+mips-linux-gnu-gcc -S -o $gcc_out $1
+
+gcc_ret=$?
+echo "mips compiler exited with code: $gcc_ret"
+
+if [ $gcc_ret -ne 0 ];
+then
+	exit 2
+fi
+
+./translate_mips2processor_demo.py $gcc_out
+
+translation_ret=$?
+echo "translation script exited with code: $translation_ret"
+
+if [ $translation_ret -ne 0 ];
+then
+	exit 3
+fi
+
+# output of translate_mips2processor_demo.py will be at main.s
+./assembler main.s
+assembly_ret=$?
+
+echo "assembler exited with code: $assembly_ret"
+
+if [ $assembly_ret -ne 0 ];
+then
+	exit 4
+fi
+
+echo "Binary file generated:"
+
+xxd -e executable.bin
+
+echo ""
+echo "Successful compilation!"
+
+exit 0
+
+
