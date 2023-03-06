6766feff8d1dabb508cb0d70eb5434cb038e73d4
RenanPicoli <renanps123@gmail.com>
2023-03-05 23:52:04 -0300

    bsp.c: trying to get ride of function overhead but mips-gcc introduces __gnu_local_gp constant and unsuported instruction lui; print_number.c: removing unecessary inline assembly

--- bsp.c
+++ bsp.c
@@ -7,6 +7,7 @@ void print_7segs(int n){
 
 //writes integer n to address addr
 //r1 stores the value, r4 stores the address
-void write_w(int addr,int n){
+/*void write_w(int addr,int n){
 	__asm("ldfp r2;\n\tlw [r2+1] r1;\n\tlw [r2+0] r4;\n\txor r3 r3 r3;\n\tadd r3 r4 r3;\n\tsw [r3+0] r1;\n\tret;\n\t");
-}
+}*/
+__asm(".globl write_w\n\t.ent write_w\n\t.type write_w, @function\n\twrite_w:\n\tldfp r2;\n\tlw [r2+1] r1;\n\tlw [r2+0] r4;\n\txor r3 r3 r3;\n\tadd r3 r4 r3;\n\tsw [r3+0] r1;\n\tret;\n\t");
--- print_number.c
+++ print_number.c
@@ -9,7 +9,6 @@ int poly(int x, int y);
 int main(void){
     int x,y;
     x = foo();
-		__asm("sw [r0+116] r2;\n\t"); //saves r2 in disp_7seg_DR mem(116) (inline assembly must follow my syntax)
 		//print_7segs(x);
 		write_w(DISPLAY_7SEGS_BASE_ADDR,x);
     while(1){
