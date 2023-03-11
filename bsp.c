df0fd939309f5763ba5e01b05669c69ae559ef09
Renan Picoli de Souza <renan.picoli@petrobras.com.br>
2023-03-10 22:13:33 -0300

    bsp.c: testing he addition of flags .remove_prologue and .remove_epilogue

--- bsp.c
+++ bsp.c
@@ -1,13 +1,16 @@
 #include "bsp.h"
 
 void print_7segs(int n){
+	__asm(".remove_prologue\n\t");
 	__asm("ldfp r2;\n\tlw [r2+0] r1;\n\txor r3 r3 r3;\n\taddi r3 r3 x\"0074\";\n\tsw [r3+0] r1;\n\tret;\n\t");
+	__asm(".remove_epilogue\n\t");
 	//__asm("ldfp r2;\n\tlw [r2+0] r1;\n\txor r3 r3 r3;\n\taddi r3 r3 DISPLAY_7SEGS_BASE_ADDR;\n\tsw [r3+0] r1;\n\tret;\n\t");
 }
 
 //writes integer n to address addr
 //r1 stores the value, r4 stores the address
-/*void write_w(int addr,int n){
+void write_w(int addr,int n){
+	__asm(".remove_prologue\n\t");
 	__asm("ldfp r2;\n\tlw [r2+1] r1;\n\tlw [r2+0] r4;\n\txor r3 r3 r3;\n\tadd r3 r4 r3;\n\tsw [r3+0] r1;\n\tret;\n\t");
-}*/
-__asm(".globl write_w\n\t.ent write_w\n\t.type write_w, @function\n\twrite_w:\n\tldfp r2;\n\tlw [r2+1] r1;\n\tlw [r2+0] r4;\n\txor r3 r3 r3;\n\tadd r3 r4 r3;\n\tsw [r3+0] r1;\n\tret;\n\t");
+	__asm(".remove_epilogue\n\t");
+}
