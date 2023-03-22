a697c41c754d80e31ad1e76fa9ce48f1669e0f00
Renan Picoli de Souza <renan.picoli@petrobras.com.br>
2023-03-22 08:46:13 -0300

    bsp.c, bsp.h: added functions to read word, copy arrays and compute the dot product between arrays

--- bsp.c
+++ bsp.c
@@ -14,3 +14,68 @@ void write_w(int addr,int n){
 	__asm("ldfp r2;\n\tlw [r2+1] r1;\n\tlw [r2+0] r4;\n\txor r3 r3 r3;\n\tadd r3 r4 r3;\n\tsw [r3+0] r1;\n\tret;\n\t");
 	__asm(".remove_epilogue\n\t");
 }
+
+//reads a single word from address addr
+int read_w(int addr){
+	__asm(".remove_prologue\n\t");
+	__asm("ldfp r30;\n\tlw [r30+0] r4;\n\tlw [r4+0] r2;\n\tpush r2;\n\tret;\n\t");
+	__asm(".remove_epilogue\n\t");
+}
+/*	ldfp r30;
+	lw [r30+0] r4;# r4 is the address argument (addr)
+	lw [r4+0] r2; # r2 receives the content pointed by r4 (*addr)
+	push r2; # saves the value read in the stack
+	ret;*/
+
+//copies l consecutive words (starting from address src_addr) to consecutives positions starting at dst_addr
+void write_multiple(int src_addr,int dst_addr,int l){
+	__asm(".remove_prologue\n\t");
+	__asm("ldfp r30;\n\tlw [r30+0] r4;\n\tlw [r30+1] r5;\n\tlw [r30+2] r6;\n\txor r1 r1 r1;\n\tLOOP:\n\tadd r4 r1 r2;\n\tadd r5 r1 r3;\n\tlw [r2+0] r8;\n\tsw [r3+0] r8;\n\taddi r1 r1 x\"0001\";\n\tbeq r1 r6 x\"0001\";\n\tjmp LOOP;\n\tret;\n\t");
+	__asm(".remove_epilogue\n\t");
+}
+/*	ldfp r30;
+	lw [r30+0] r4;# src_addr
+	lw [r30+1] r5;# dst_addr
+	lw [r30+2] r6;# length
+	xor r1 r1 r1;# iteration counter
+LOOP:
+	add r4 r1 r2;# pointer to src
+	add r5 r1 r3;# pointer to dst
+	lw [r2+0] r8;# copies the data to r8
+	sw [r3+0] r8;# stores the data in the destination
+	addi r1 r1 x"0001";
+	beq r1 r6 x"0001";
+	jmp LOOP;
+	ret;*/
+
+
+//copies 8 consecutive words - aka vector - (starting from address src_addr) to consecutives positions starting at dst_addr
+//intended to use for manually filling a peripheral that operates on vectors
+void write_vector(int src_addr,int dst_addr){
+	__asm(".remove_prologue\n\t");
+	__asm("ldfp r30;\n\tlw [r30+0] r4;\n\tlw [r30+1] r5;\n\txor r6 r6 r6;\n\taddi r6 r6 x\"0008\";\n\txor r1 r1 r1;\n\tLOOP:\n\tadd r4 r1 r2;\n\tadd r5 r1 r3;\n\tlw [r2+0] r8;\n\tsw [r3+0] r8;\n\taddi r1 r1 x\"0001\";\n\tbeq r1 r6 x\"0001\";\n\tjmp LOOP;\n\tret;\n\t");
+	__asm(".remove_epilogue\n\t");
+}
+/*	ldfp r30;
+	lw [r30+0] r4;# src_addr
+	lw [r30+1] r5;# dst_addr
+	xor r6 r6 r6;
+	addi r6 r6 x"0008";# length
+	xor r1 r1 r1;# iteration counter
+LOOP:
+	add r4 r1 r2;# pointer to src
+	add r5 r1 r3;# pointer to dst
+	lw [r2+0] r8;# copies the data to r8
+	sw [r3+0] r8;# stores the data in the destination
+	addi r1 r1 x"0001";
+	beq r1 r6 x"0001";
+	jmp LOOP;
+	ret;*/
+
+//computes the dot product between two arrays of l elements	
+float dot_product(int A_ptr,int B_ptr,uint l){
+	write_multiple(A_ptr,INNER_PRODUCT_BASE_ADDR+INNER_PRODUCT_A_OFFSET,l);
+	write_multiple(B_ptr,INNER_PRODUCT_BASE_ADDR+INNER_PRODUCT_B_OFFSET,l);
+	float result=read_w(INNER_PRODUCT_BASE_ADDR+INNER_PRODUCT_RESULT_OFFSET);//should I cast to float???
+	return result;
+}
--- bsp.h
+++ bsp.h
@@ -1,6 +1,11 @@
 //function prototypes
 void print_7segs(int n);
 void write_w(int addr,int n);
+int read_w(int addr);
+void write_multiple(int src_addr,int dst_addr,int l);
+void write_vector(int src_addr,int dst_addr);
+
+float dot_product(int A_ptr,int B_ptr,uint l);
 
 //constants
 
@@ -22,33 +27,33 @@ void write_w(int addr,int n);
 #define PROGRAM_STACK_BASE_ADDR			0x200
 
 //peripheral offsets
-#define FILTER_COEFFS_OFFSET				0x00
-#define FILTER_XN_OFFSET					0x00
-#define CACHE_OFFSET 						0x00
-#define INNER_PRODUCT_A_OFFSET				0x00
-#define INNER_PRODUCT_B_OFFSET				0x08
-#define INNER_PRODUCT_RESULT_OFFSET			0x10
-#define VMAC_BASE_A_OFFSET					0x00
-#define VMAC_BASE_B_OFFSET					0x08
-#define VMAC_BASE_RESULT_OFFSET				0x10
-#define I2C_CR_OFFSET 						0x00
-#define I2C_DR_OFFSET 						0x01
-#define I2C_IRQ_CTRL_OFFSET 				0x04
-#define I2S_CR_OFFSET 						0x00
-#define I2S_DR_OFFSET 						0x01
-#define I2S_SR_OFFSET 						0x02
-#define I2S_IRQ_CTRL_OFFSET 				0x04
-#define FILTER_OUTPUT_OFFSET 				0x00
-#define DESIRED_RESPONSE_OFFSET  			0x00
-#define FILTER_CTRL_STATUS_OFFSET 			0x00
-#define CONVERTED_OUTPUT_OFFSET 			0x00
-#define DISPLAY_7SEGS_OFFSET 				0x00
-#define IRQ_CTRL_IRQ_PEND_OFFSET			0x00
-#define IRQ_CTRL_IRQ_ACTIVE_OFFSET			0x01
-#define IRQ_CTRL_IRQ_STARTED_OFFSET			0x02
-#define IRQ_CTRL_IRQ_STATUS_OFFSET			0x03
-#define IRQ_CTRL_IRQ_CURR_OUT_OFFSET		0x04
-#define IRQ_CTRL_VECTOR_OFFSET				0x20
-#define IRQ_CTRL_PRIORITIES_OFFSET			0x40
+#define FILTER_COEFFS_OFFSET			0x00
+#define FILTER_XN_OFFSET				0x00
+#define CACHE_OFFSET 					0x00
+#define INNER_PRODUCT_A_OFFSET			0x00
+#define INNER_PRODUCT_B_OFFSET			0x08
+#define INNER_PRODUCT_RESULT_OFFSET		0x10
+#define VMAC_BASE_A_OFFSET				0x00
+#define VMAC_BASE_B_OFFSET				0x08
+#define VMAC_BASE_RESULT_OFFSET			0x10
+#define I2C_CR_OFFSET 					0x00
+#define I2C_DR_OFFSET 					0x01
+#define I2C_IRQ_CTRL_OFFSET 			0x04
+#define I2S_CR_OFFSET 					0x00
+#define I2S_DR_OFFSET 					0x01
+#define I2S_SR_OFFSET 					0x02
+#define I2S_IRQ_CTRL_OFFSET 			0x04
+#define FILTER_OUTPUT_OFFSET 			0x00
+#define DESIRED_RESPONSE_OFFSET  		0x00
+#define FILTER_CTRL_STATUS_OFFSET 		0x00
+#define CONVERTED_OUTPUT_OFFSET 		0x00
+#define DISPLAY_7SEGS_OFFSET 			0x00
+#define IRQ_CTRL_IRQ_PEND_OFFSET		0x00
+#define IRQ_CTRL_IRQ_ACTIVE_OFFSET		0x01
+#define IRQ_CTRL_IRQ_STARTED_OFFSET		0x02
+#define IRQ_CTRL_IRQ_STATUS_OFFSET		0x03
+#define IRQ_CTRL_IRQ_CURR_OUT_OFFSET	0x04
+#define IRQ_CTRL_VECTOR_OFFSET			0x20
+#define IRQ_CTRL_PRIORITIES_OFFSET		0x40
 #define INSTRUCTION_MEMORY_OFFSET			0x00
 #define PROGRAM_STACK_OFFSET				0x00
