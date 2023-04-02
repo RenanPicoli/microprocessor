42301b860fb0960d9e3907c473a00794a6eaec84
RenanPicoli <renanps123@gmail.com>
2023-04-01 23:08:46 -0300

    print_number.c: trying to print a big constant (>= 2^16) to test lui instruction

--- print_number.c
+++ print_number.c
@@ -9,8 +9,9 @@ int poly(int x, int y);
 int main(void){
     int x,y;
     x = foo();
+		y=720600004;
 		//print_7segs(x);
-		write_w(DISPLAY_7SEGS_BASE_ADDR,x<<2);
+		write_w(DISPLAY_7SEGS_BASE_ADDR,y<<2);
 		int dst_mask=LVEC_DST_MSK(1);
 		LVEC(x"05",x"02");
 		//LVEC(x"05",LVEC_DST_MSK_1);
