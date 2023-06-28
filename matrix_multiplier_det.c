ea0bffec710ed0acf6f7eef9e3ef884abef436a3
RenanPicoli <renanps123@gmail.com>
2023-06-28 20:45:15 -0300

    matrix_multiplier_det.c: using register keyword to sugest the compiler to put some variables inside registers instead of ram (stack). this decreased the size of executable. The music still plays very loud and noisy

--- matrix_multiplier_det.c
+++ matrix_multiplier_det.c
@@ -60,15 +60,15 @@ int main(void){
 	word (*B_w)[2] = B;
 	word (*C_w)[2] = C;
 	
-	for(int i=0;i<2;i++){//iterates through lines of A
-		for(int j=0;j<2;j++){//iterates through columns of B
+	for(register int i=0;i<2;i++){//iterates through lines of A
+		for(register int j=0;j<2;j++){//iterates through columns of B
 			//copies one line of A and one column of B to inner_product
-			for(int k=0;k<2;k++){
+			for(register int k=0;k<2;k++){
 				write_w(INNER_PRODUCT_BASE_ADDR+INNER_PRODUCT_A_OFFSET+k*(sizeof(int)),A_w[i][k].i);
 				write_w(INNER_PRODUCT_BASE_ADDR+INNER_PRODUCT_B_OFFSET+k*(sizeof(int)),B_w[k][j].i);
 			}
 			//fills with zeroes the unused positions
-			for(int k=2;k<8;k++){
+			for(register int k=2;k<8;k++){
 				write_w(INNER_PRODUCT_BASE_ADDR+INNER_PRODUCT_A_OFFSET+k*(sizeof(int)),0);
 				write_w(INNER_PRODUCT_BASE_ADDR+INNER_PRODUCT_B_OFFSET+k*(sizeof(int)),0);
 			}
@@ -86,7 +86,6 @@ int main(void){
 	print_7segs(det_w.i);
 	
 	GIC_config();
-	
 	filter_control(0);//disables filter
 	
 	I2S_Init_typedef i2s;
@@ -104,7 +103,7 @@ int main(void){
 	codec_init();
 	
 	filter_control(1);//enables filter	
-	
+
     while(1){
         HALT();
     }
@@ -163,7 +162,7 @@ void GIC_config(){
 	return;
 }
 
-//condifgures the audio codec
+//configures the audio codec
 void codec_init(){
 	I2C_Init_typedef i2c;
 	i2c.num_words = I2C_WORDS_2;
@@ -202,17 +201,17 @@ void IRQ0_Handler(){
 	//LVEC(2,LVEC_DST_MSK_3|LVEC_DST_MSK_4|LVEC_DST_MSK_6);
 	LVEC(x"02",x"58");
 	WRITE(INNER_PRODUCT_BASE_ADDR+INNER_PRODUCT_CTRL_OFFSET,0x1);
-	word squared_norm_w;
+	register word squared_norm_w;
 	READ(INNER_PRODUCT_BASE_ADDR+INNER_PRODUCT_RESULT_OFFSET,squared_norm_w.i);
-	word half_inv_sqr_norm_w;
+	register word half_inv_sqr_norm_w;
 	FDIV(0.5f,squared_norm_w.f,half_inv_sqr_norm_w.f);
 	float step=min(half_inv_sqr_norm_w.f,1e4f);//f for float
 	//float step=min(0.5/squared_norm_w.f,1e4f);//f for float
-	word step_w;
+	register word step_w;
 	FMUL(2.0f,step,step_w.f);//step_w.f = 2.0f*step;
 	WRITE(CACHE_BASE_ADDR+0,step_w.i);
 	
-	word desired_w;
+	register word desired_w;
 	READ(DESIRED_RESPONSE_BASE_ADDR+DESIRED_RESPONSE_OFFSET,desired_w.i);
 	WRITE(CACHE_BASE_ADDR+4,desired_w.i);//saves desired response to position 1 of mini_ram
 	
@@ -228,22 +227,22 @@ void IRQ1_Handler(){
     __asm(".remove_epilogue\n\t");
 }
 
-// handler of IRQ0 (filter_CLK falling_edge)
+// handler of IRQ3 (filter_CLK falling_edge)
 void IRQ3_Handler(){
     __asm(".remove_prologue\n\t");
-	word filter_out_w;
+	register word filter_out_w;
 	READ(FILTER_OUTPUT_BASE_ADDR+FILTER_OUTPUT_OFFSET,filter_out_w.i);
 
-	word desired_w;
+	register word desired_w;
 	READ(CACHE_BASE_ADDR+4,desired_w.i);
 	
-	word error_w;
+	register word error_w;
 	FSUB(desired_w.f,filter_out_w.f,error_w.f);//error_w.f=desired_w.f-filter_out_w.f;
 	
-	word double_step_w;//2*step
+	register word double_step_w;//2*step
 	READ(CACHE_BASE_ADDR+0,double_step_w.i);
 	
-	word lambda_w;
+	register word lambda_w;
 	FMUL(double_step_w.f,error_w.f,lambda_w.f);//lambda_w.f=double_step_w.f*error_w.f; (2*step*error)
 	WRITE(VMAC_BASE_ADDR+VMAC_LAMBDA_OFFSET,lambda_w.i);
 	
@@ -261,12 +260,12 @@ void IRQ3_Handler(){
 	//I2S transmission (left fifo já foi selecionada antes do loop principal)
 	//escreve 2x no DR (upsampling fator 2, 22050 Hz -> 44100 Hz)
 	//habilita a transmissão
-	int converted_output;
+	register int converted_output;
 	READ(CONVERTED_OUTPUT_BASE_ADDR+CONVERTED_OUTPUT_OFFSET,converted_output);
 	WRITE(I2S_BASE_ADDR+I2S_DR_OFFSET,converted_output);
 	WRITE(I2S_BASE_ADDR+I2S_DR_OFFSET,converted_output);
 	
-	int i2s_cfg;
+	register int i2s_cfg;
 	READ(I2S_BASE_ADDR+I2S_CR_OFFSET,i2s_cfg);
 	i2s_cfg |= I2S_EN;
 	WRITE(I2S_BASE_ADDR+I2S_CR_OFFSET,i2s_cfg);
