cbd3486eed6d013fdf5309ef15ed94d162d2b88c
RenanPicoli <renanps123@gmail.com>
2023-07-30 23:02:47 -0300

    print_number.c: refactoring code to test lcd library

--- print_number.c
+++ print_number.c
@@ -1,5 +1,6 @@
 #include "cpu.h"
 #include "bsp.h"
+#include "lcd.h"
 
 //function prototypes
 int foo(void);
@@ -8,39 +9,18 @@ int poly(int x, int y);
 
 int main(void){
     int x,y;
-    x = foo();
-		y=720600004;
-		//print_7segs(x);
-		write_w(DISPLAY_7SEGS_BASE_ADDR,y<<2);
-		int dst_mask=LVEC_DST_MSK(1);
-		LVEC(x"05",x"02");
-		//LVEC(x"05",LVEC_DST_MSK_1);
-		//lvec_f(5,dst_mask);
-		VMAC();
+	y=720600004;
+	//print_7segs(x);
+	write_w(DISPLAY_7SEGS_BASE_ADDR,y<<2);
+	lcd_clear();
+	lcd_set_ddram_addr(0);
+	lcd_write_data('A');
+	lcd_write_data('b');
     while(1){
-        //printf("Hello World! x=%d\n",x);
-        //y=sum(x,1);
-        //x=poly(x,y);
     }
     return 0;
 }
 
-int foo(void) {
-    int a=3;
-    int b=4;
-    if(a*b > 20){
-        return sum(a,b);
-    }else{
-        return poly(a,b);
-    }
-}
-
-int poly(int x, int y) {
-    return (x * (sum(x,y)));
-}
-
 #include "cpu.c"
 #include "bsp.c"
-//int sum(int x, int y) {
-//    return x+y;
-//}
+#include "lcd.c"
