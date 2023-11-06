6fa0db2c33bd35bc1ded9fd2095393554479a793
RenanPicoli <renanps123@gmail.com>
2023-11-05 22:11:21 -0300

    adaptive_filter_LCD.c: using only register attribute in IRQ3_Handler(), trying to force the least usage of program stack for speed

--- adaptive_filter_LCD.c
+++ adaptive_filter_LCD.c
@@ -3,8 +3,6 @@
 #include "wm8731.h"
 #include "lcd.h"
 
-void register_init();
-
 //configures the global interrupt controller
 void GIC_config();
 
@@ -33,7 +31,6 @@ float min(float x, float y);
 
 //main loop
 int main(void){
-    register_init();
 	GIC_config();
 	//lcd_print_task = &IRQ4_Handler;
 	//(*lcd_print_task)();
@@ -63,44 +60,6 @@ int main(void){
         HALT();
     }
     return 0;
-}
-
-void register_init(){
-__asm(".remove_prologue\n\t\
-    xor r0 r0 r0; zera r0\n\t\
-    xor r1 r1 r1; zera r1\n\t\
-    xor r2 r2 r2; zera r2\n\t\
-    xor r3 r3 r3; zera r3\n\t\
-    xor r4 r4 r4; zera r4\n\t\
-    xor r5 r5 r5; zera r5\n\t\
-    xor r6 r6 r6; zera r6\n\t\
-    xor r7 r7 r7; zera r7\n\t\
-    xor r8 r8 r8; zera r8\n\t\
-    xor r9 r9 r9; zera r9\n\t\
-    xor r10 r10 r10; zera r10\n\t\
-    xor r11 r11 r11; zera r11\n\t\
-    xor r12 r12 r12; zera r12\n\t\
-    xor r13 r13 r13; zera r13\n\t\
-    xor r14 r14 r14; zera r14\n\t\
-    xor r15 r15 r15; zera r15\n\t\
-    xor r16 r16 r16; zera r16\n\t\
-    xor r17 r17 r17; zera r17\n\t\
-    xor r18 r18 r18; zera r18\n\t\
-    xor r19 r19 r19; zera r19\n\t\
-    xor r20 r20 r20; zera r20\n\t\
-    xor r21 r21 r21; zera r21\n\t\
-    xor r22 r22 r22; zera r22\n\t\
-    xor r23 r23 r23; zera r23\n\t\
-    xor r24 r24 r24; zera r24\n\t\
-    xor r25 r25 r25; zera r25\n\t\
-    xor r26 r26 r26; zera r26\n\t\
-    xor r27 r27 r27; zera r27\n\t\
-    xor r28 r28 r28; zera r28\n\t\
-    xor r29 r29 r29; zera r29\n\t\
-    xor r30 r30 r30; zera r30\n\t\
-    xor r31 r31 r31; zera r31\n\t\
-	ret;\n\t\
-    .remove_epilogue\n\t");
 }
 
 //configures the global interrupt controller
@@ -190,19 +149,19 @@ void IRQ1_Handler(){
 // handler of IRQ0 (filter_CLK falling_edge)
 void IRQ3_Handler(){
     __asm(".remove_prologue\n\t");
-	word filter_out_w;
+	register word filter_out_w;
 	READ(FILTER_OUTPUT_BASE_ADDR+FILTER_OUTPUT_OFFSET,filter_out_w.i);
 
-	word desired_w;
+	register word desired_w;
 	READ(CACHE_BASE_ADDR+1*4,desired_w.i);
 	
-	word error_w;
+	register word error_w;
 	FSUB(desired_w.f,filter_out_w.f,error_w.f);//error_w.f=desired_w.f-filter_out_w.f;
 	
-	word double_step_w;//2*step
+	register word double_step_w;//2*step
 	READ(CACHE_BASE_ADDR+0*4,double_step_w.i);
 	
-	word lambda_w;
+	register word lambda_w;
 	FMUL(double_step_w.f,error_w.f,lambda_w.f);//lambda_w.f=double_step_w.f*error_w.f; (2*step*error)
 	WRITE(VMAC_BASE_ADDR+VMAC_LAMBDA_OFFSET,lambda_w.i);
 	
@@ -214,7 +173,7 @@ void IRQ3_Handler(){
 	//Lê memória de coeficientes do filtro(0) para o filtro(1), produto interno (A e B - 3 e 4)
 	LVECR(0,26);
 	WRITE(INNER_PRODUCT_BASE_ADDR+INNER_PRODUCT_CTRL_OFFSET,0x1);
-	word squared_norm_coeffs_w;//squared norm of filter coefficients
+	register word squared_norm_coeffs_w;//squared norm of filter coefficients
 	READ(INNER_PRODUCT_BASE_ADDR+INNER_PRODUCT_RESULT_OFFSET,squared_norm_coeffs_w.i);
 	
 	//TODO: se filtro já convergiu, sair do loop
@@ -222,31 +181,31 @@ void IRQ3_Handler(){
 	//I2S transmission (left fifo já foi selecionada antes do loop principal)
 	//escreve 2x no DR (upsampling fator 2, 22050 Hz -> 44100 Hz)
 	//habilita a transmissão
-	int converted_output;
+	register int converted_output;
 	READ(CONVERTED_OUTPUT_BASE_ADDR+CONVERTED_OUTPUT_OFFSET,converted_output);
 	WRITE(I2S_BASE_ADDR+I2S_DR_OFFSET,converted_output);
 	WRITE(I2S_BASE_ADDR+I2S_DR_OFFSET,converted_output);
 	
-	int i2s_cfg;
+	register int i2s_cfg;
 	READ(I2S_BASE_ADDR+I2S_CR_OFFSET,i2s_cfg);
 	i2s_cfg |= I2S_EN;
 	WRITE(I2S_BASE_ADDR+I2S_CR_OFFSET,i2s_cfg);
 		
-	word squared_lambda_w;
+	register word squared_lambda_w;
 	//computes squared_lambda_w
 	FMUL(lambda_w.f,lambda_w.f,squared_lambda_w.f);
-	word squared_norm_w;
+	register word squared_norm_w;
 	READ(CACHE_BASE_ADDR+2*4,squared_norm_w.i);
 	FMUL(squared_lambda_w.f,squared_norm_w.f,squared_norm_w.f);
 
-	float squared_dev;//filter coefficients SQUARED deviation
+	register float squared_dev;//filter coefficients SQUARED deviation
 	FDIV(squared_norm_w.f,squared_norm_coeffs_w.f,squared_dev);
 
 	//saves the squared deviation of filter coefficients to cache, position 3
 	//WRITE(CACHE_BASE_ADDR+3*4,squared_dev);
 	   
 	//tests if filter converged
-	word d_w;
+	register word d_w;
 	FSUB(squared_dev,1e-13f,d_w.f);
 	//d_w.i &= 0x80000000;
 	if((d_w.i & 0x80000000)==0x80000000){//(squared_dev - 1e-13f)<0
