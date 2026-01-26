3c0ef7cba89a5400b94a99e18ec631533b04acc8
RenanPicoli <renanps123@gmail.com>
2026-01-26 02:09:08 -0300

    test_VGA.c: added function to drawa single pixel in VGA and do to demonstrate other drawing functions

--- test_VGA.c
+++ test_VGA.c
@@ -9,16 +9,22 @@ void GIC_config();
 // handler of IRQ5 (DMA interrupt)
 void IRQ5_Handler();
 
+void demo(uint32_t *fb);
+
 //main loop
 int main(void){
 	DMA_Init_typedef dma_init;
-    dma_init.num_xfers = 640*480;//length of the image in pixels (each pixel is stored as a 32-bit word)
+    dma_init.num_xfers = VGA_WIDTH * VGA_HEIGHT;//length of the image in pixels (each pixel is stored as a 32-bit word)
+
     dma_init.src_addr = SDRAM_BASE_ADDR;
-    dma_init.dst_addr = VGA_BASE_ADDR+VGA_DR_OFFSET;
+    dma_init.dst_addr = VGA_BASE_ADDR + VGA_DR_OFFSET;
+
     dma_init.dinc_select = DMA_DINC_DISABLE;//writes to the fixed position in VGA
     // dma_init.sinc_select = DMA_SINC_DISABLE;//reads always the first position in SDRAM (fills with single color)
     dma_init.sinc_select = DMA_SINC_ENABLE;// enables increment of address in SDRAM (loads an entire image)
+
     dma_init.src_lat_select = DMA_SRC_LAT_3;//source memory (SDRAM) has 2 clocks of latency (only for reading) + 1 of DMA
+
     dma_init.autostart_select = DMA_AUTOSTART_ENABLE;//will repeat tranfer after finishing
     DMA_Init(&dma_init);
 
@@ -28,6 +34,8 @@ int main(void){
     //no direct blanking
     //no sync on green
     WRITE(VGA_BASE_ADDR+VGA_CR_OFFSET,0x3);
+
+    demo((uint32_t *)SDRAM_BASE_ADDR);
 
     DMA_start();
 	
@@ -57,6 +65,34 @@ void IRQ5_Handler(){
 
 	IRET();
     __asm(".remove_epilogue\n\t");
+}
+
+void VGA_put_pixel(uint32_t *fb,
+                             int x, int y,
+                             uint32_t color)
+{
+    if (x < 0 || x >= VGA_WIDTH ||
+        y < 0 || y >= VGA_HEIGHT)
+        return;
+    fb[y * VGA_WIDTH + x] = color;
+}
+
+void demo(uint32_t *fb)
+{
+    //  VGA_put_pixel(fb, 320, 240, 0x002CFF05);//neon-green
+
+    // Limpa a tela
+    // VGA_fill_rect(fb, 0, 0, VGA_WIDTH, VGA_HEIGHT, 0x00000000);
+
+    // Retângulo cheio
+    // VGA_fill_rect(fb, 320, 240, 160, 80, 0x002CFF05);//neon-green
+
+    // Contorno
+    // VGA_draw_rect(fb, 320, 240, 160, 80, 0x000000FF);//blue
+
+    // Linhas cruzadas
+    VGA_draw_line(fb, 0, 0, 639, 479, 0x00FF0000);//red
+    // VGA_draw_line(fb, 639, 0, 0, 479, 0x000000FF);//green
 }
 
 
