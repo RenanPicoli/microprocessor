583161f1e59514fcf6d33f22d401d39507813106
RenanPicoli <renanps123@gmail.com>
2026-05-05 00:19:05 -0300

    assembler.c: updated instruction memory base address (where the program starts in RAM memory)

--- assembler.c
+++ assembler.c
@@ -33,7 +33,7 @@
 #define MAX_STR_LENGTH 200
 
 // byte address
-#define INSTRUCTION_MEMORY_BASE_ADDR 0x2000
+#define INSTRUCTION_MEMORY_BASE_ADDR 0x20000
 
 //for the opcode dictionary
 typedef struct
