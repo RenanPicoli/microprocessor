188920ecd78fef26d37bcbc10d47cec8662b9fcb
RenanPicoli <renanps123@gmail.com>
2026-05-08 04:05:12 -0300

    test_VGA.c: testing using DMA to read framebuffer to mini_ram cache, then cpu to write char, the DMA to write it back to framebuffer. But DMA is not switching to writing state (state=10)

--- test_VGA.c
+++ test_VGA.c
@@ -14,7 +14,20 @@ void demo(uint32_t *fb);
 
 //main loop
 int main(void){
-	DMA_Init_typedef dma_init;
+
+	GIC_config();
+	
+    //configure VGA controller
+    //no direct blanking
+    //no sync on green
+    WRITE(VGA_BASE_ADDR+VGA_CR_OFFSET,0x3);
+
+    //draws over image on framebuffer, prints characters
+    demo((uint32_t *)SDRAM_BASE_ADDR);
+
+    DMA_Init_typedef dma_init;
+
+    // configures DMA for continuously transmitting the framebufer to VGA
     dma_init.num_xfers = VGA_WIDTH * VGA_HEIGHT;//length of the image in pixels (each pixel is stored as a 32-bit word)
 
     dma_init.src_addr = SDRAM_BASE_ADDR;
@@ -28,15 +41,6 @@ int main(void){
 
     dma_init.autostart_select = DMA_AUTOSTART_ENABLE;//will repeat tranfer after finishing
     DMA_Init(&dma_init);
-
-	GIC_config();
-	
-    //configure VGA controller
-    //no direct blanking
-    //no sync on green
-    WRITE(VGA_BASE_ADDR+VGA_CR_OFFSET,0x3);
-
-    demo((uint32_t *)SDRAM_BASE_ADDR);
 
     VGA_start();
 
@@ -71,34 +75,60 @@ void IRQ5_Handler(){
 }
 
 void print_vga(){
-    // uint32_t row0[]={0,0,0,0xfffff,0xfffff,0,0,0};
-    // uint32_t row1[]={0,0,0,0xfffff,0xfffff,0xfffff,0,0};
-    // uint32_t row2[]={0,0,0xfffff,0xfffff,0,0,0xfffff,0};
-    // uint32_t row3[]={0,0xfffff,0xfffff,0xfffff,0xfffff,0xfffff,0xfffff,0xfffff};
-    // uint32_t row4[]={0,0xfffff,0xfffff,0xfffff,0xfffff,0xfffff,0xfffff,0xfffff};
-    // uint32_t row5[]={0,0xfffff,0xfffff,0,0,0,0xfffff,0xfffff};
-    // uint32_t row6[]={0,0xfffff,0xfffff,0,0,0,0xfffff,0xfffff};
-    // uint32_t row7[]={0,0xfffff,0xfffff,0,0,0,0xfffff,0xfffff};
     
     uint32_t* fb = (uint32_t *)SDRAM_BASE_ADDR;
     int base_x=300;
     int base_y=70;
-    //print letter 'A'
-    // for (int i=0;i<8;i++){
-    //     VGA_put_pixel(fb,x+i,y+0,row0[i]);
-    //     VGA_put_pixel(fb,x+i,y+1,row1[i]);
-    //     VGA_put_pixel(fb,x+i,y+2,row2[i]);
-    //     VGA_put_pixel(fb,x+i,y+3,row3[i]);
-    //     VGA_put_pixel(fb,x+i,y+4,row4[i]);
-    //     VGA_put_pixel(fb,x+i,y+5,row5[i]);
-    //     VGA_put_pixel(fb,x+i,y+6,row6[i]);
-    //     VGA_put_pixel(fb,x+i,y+7,row7[i]);
-    // }
+    uint32_t font_color = (uint32_t)0x00ff0000;
 
-    const Glyph *g = ascii_get_glyph('A');
-    for (int i = 0; i < g->count; i++){
-        VGA_put_pixel((uint32_t*)fb,base_x+g->points[i].x,base_y+g->points[i].y,(uint32_t)0x00ff0000);
+	DMA_Init_typedef dma_init;
+
+    //configures DMA to perform single transfers
+    dma_init.num_xfers = FONT_WIDTH;//length of a single row of a character
+
+    //leaves dma_init.src_addr and dma_init.dst_addr zeroed for now
+
+    dma_init.dinc_select = DMA_DINC_ENABLE;//increments destination address (SDRAM framebuffer)
+    dma_init.sinc_select = DMA_SINC_ENABLE;//increment source address (glyph stored in mini_ram)
+
+    dma_init.src_lat_select = DMA_SRC_LAT_1;//source memory (mini_ram registers) has 0 clock of latency (only for reading) + 1 of DMA
+
+    dma_init.autostart_select = DMA_AUTOSTART_DISABLE;//performs single transfer
+    //prints letter 'A'
+    char c = 'A';
+    
+    // iterates over rows of region to be drawn
+    for (int y=base_y; y < base_y+FONT_HEIGHT; y++){
+        //fill mini_ram cache with a row (8 pixels) where the char will be drawn, uses DMA
+        //byte address of row start
+        dma_init.src_addr = (uint32_t) fb + (y * VGA_WIDTH * sizeof(uint32_t)) + (sizeof(uint32_t) * base_x);
+        dma_init.dst_addr = CACHE_BASE_ADDR + CACHE_OFFSET;
+        DMA_Init_and_Start(&dma_init);
+        HALT(); //cpu sleeps until IRQ (DMA transfer finished)
+
+        //iterate over pixels on mini_ram replacing with font_color where necessary; j is the column number
+        // j: 0 1 ... 7 (pixel number)
+        // i: 7 6 ... 0 (bit index of mask)
+        for (int j=0;j<FONT_WIDTH;j++){
+            uint32_t pixel = read_w(CACHE_BASE_ADDR + j*sizeof(uint32_t));//original pixel
+            uint32_t row = font_bitmap[c - ASCII_FIRST][y - base_y];//single row of font_bitmaps, bit 1 corresponds to drawn pixel
+            uint32_t bit_j = row & (1 << (FONT_WIDTH - 1 - j));// non-zero only if column j of this row belongs to character
+            uint32_t mask = (bit_j == 0) ? 0x00000000 : 0xffffffff;// 11...1 only if column j of this row belongs to character
+            pixel = (pixel & (~mask)) | (font_color & mask);// new pixel
+            write_w(CACHE_BASE_ADDR + j*sizeof(uint32_t), pixel);// store new pixel to mini_ram cache
+        }
+
+        //sends the row back to framebuffer with DMA
+        dma_init.src_addr = CACHE_BASE_ADDR + CACHE_OFFSET;
+        //byte address of row start
+        dma_init.dst_addr = (uint32_t) fb + (y * VGA_WIDTH * sizeof(uint32_t)) + (sizeof(uint32_t) * base_x);
+        DMA_Init_and_Start(&dma_init);
+        HALT(); //cpu sleeps until IRQ (DMA transfer finished)
     }
+    // const Glyph *g = ascii_get_glyph('A');
+    // for (int i = 0; i < g->count; i++){
+    //     VGA_put_pixel((uint32_t*)fb,base_x+g->points[i].x,base_y+g->points[i].y,font_color);
+    // }
 
 
     return;
