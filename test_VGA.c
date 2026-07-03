44dbb85de0fdef68f6cad20b73ea3ec00fe91475
RenanPicoli <renanps123@gmail.com>
2026-07-03 03:07:22 -0300

    test_VGA.c: changed SDRAM latency setting to 4 and this fixed the DMA transfer from SDRAM to mini_ram

--- test_VGA.c
+++ test_VGA.c
@@ -38,7 +38,7 @@ int main(void){
     // dma_init.sinc_select = DMA_SINC_DISABLE;//reads always the first position in SDRAM (fills with single color)
     dma_init.sinc_select = DMA_SINC_ENABLE;// enables increment of address in SDRAM (loads an entire image)
 
-    dma_init.src_lat_select = DMA_SRC_LAT_3;//source memory (SDRAM) has 2 clocks of latency (only for reading) + 1 of DMA
+    dma_init.src_lat_select = DMA_SRC_LAT_4;//source memory (SDRAM) has 2 clocks of latency (only for reading) + 2 of DMA/arbitration
 
     dma_init.autostart_select = DMA_AUTOSTART_ENABLE;//will repeat tranfer after finishing
     DMA_Init(&dma_init);
@@ -88,10 +88,10 @@ void print_vga(uint32_t* fb){
 
     //leaves dma_init.src_addr and dma_init.dst_addr zeroed for now
 
-    dma_init.dinc_select = DMA_DINC_ENABLE;//increments destination address (SDRAM framebuffer)
-    dma_init.sinc_select = DMA_SINC_ENABLE;//increment source address (glyph stored in mini_ram)
+    dma_init.dinc_select = DMA_DINC_ENABLE;//increment destination address (glyph stored in mini_ram)
+    dma_init.sinc_select = DMA_SINC_ENABLE;//increment source address (SDRAM framebuffer)
 
-    dma_init.src_lat_select = DMA_SRC_LAT_3;//source memory (SDRAM) has 2 clock of latency (only for reading) + 1 of DMA
+    dma_init.src_lat_select = DMA_SRC_LAT_4;//source memory (SDRAM) has 2 clock of latency (only for reading) + 2 of DMA/arbitration
 
     dma_init.autostart_select = DMA_AUTOSTART_DISABLE;//performs single transfer
     //prints letter 'A'
@@ -121,6 +121,7 @@ void print_vga(uint32_t* fb){
         }
 
         //sends the row back to framebuffer with DMA
+        dma_init.src_lat_select = DMA_SRC_LAT_0;
         dma_init.src_addr = CACHE_BASE_ADDR + CACHE_OFFSET;
         //byte address of row start
         dma_init.dst_addr = (uint32_t) fb + (y * VGA_WIDTH * sizeof(uint32_t)) + (sizeof(uint32_t) * base_x);
@@ -151,7 +152,7 @@ void demo(uint32_t *fb)
     dma_init.dinc_select = DMA_DINC_ENABLE;//increments destination address (SDRAM framebuffer)
     dma_init.sinc_select = DMA_SINC_ENABLE;//increment source address (SDRAM read-only region)
 
-    dma_init.src_lat_select = DMA_SRC_LAT_3;//source memory (SDRAM) has 2 clock of latency (only for reading) + 1 of DMA
+    dma_init.src_lat_select = DMA_SRC_LAT_4;//source memory (SDRAM) has 2 clock of latency (only for reading) + 2 of DMA/arbitration
     dma_init.autostart_select = DMA_AUTOSTART_DISABLE;//performs single transfer
     
     DMA_Init_and_Start(&dma_init);
